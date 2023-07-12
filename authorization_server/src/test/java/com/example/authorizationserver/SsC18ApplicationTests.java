3d2e56d7c0102eb38440b9a1705529e2a8a372e2
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2023-07-11 23:52:18 -0300

    init

---
+++ authorization_server/src/test/java/com/example/authorizationserver/SsC18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
